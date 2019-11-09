541983da942a3bbec4396f41ff4a9924b6f5716e
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-09 17:12:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,7 @@ def key_for_min_value(name_hash)
   key = nil
   counts = name_hash.collect { |name, count| count }
   lowest_count = counts.first
+  
   name_hash.each do |name, count|
     if count < lowest_count
       lowest_count = count
