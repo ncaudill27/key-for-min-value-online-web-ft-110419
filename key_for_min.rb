355d67ce69ad69653b3fa4fa2db91ffde8af27cc
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-09 16:04:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,9 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+require 'pry'
 
 def key_for_min_value(name_hash)
-
+  values = name_hash.collect { |name, value| value }
+  lowest_value = value.first
+  lowest_value
 end
